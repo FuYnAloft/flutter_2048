a8cb55c4d671bd6bbf3399d5f641aef093daf14c
FuYnAloft <207443530+fuynaloft@users.noreply.github.com>
2026-02-28 19:00:27 +0800

    init commit

---
+++ android/app/src/main/kotlin/dev/fuyn/flutter_2048/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.fuyn.flutter_2048
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/model/tile_entity.dart
@@ -0,0 +1,42 @@
+﻿import 'dart:math';
+
+enum AnimationState { normal, merged }
+
+class TileEntity {
+  final int id;
+  final int value; // 1 -> 2, 2 -> 4, 3 -> 8, ...
+  TileEntity? delegate;
+  int _row;
+  int _column;
+  AnimationState animationState = .normal;
+
+  int get row => delegate?._row ?? _row;
+
+  set row(int other) => _row = other;
+
+  int get column => delegate?._column ?? _column;
+
+  set column(int other) => _column = other;
+
+  TileEntity(this.id, this.value, this._row, this._column, [this.delegate]);
+
+  TileEntity? mergeWith(TileEntity other) {
+    if (value != other.value) return null;
+    final merged = TileEntity(id, value + 1, row, column);
+    animationState = .merged;
+    other.animationState = .merged;
+    delegate = merged;
+    other.delegate = merged;
+    return merged;
+  }
+
+  void moveTo(int newRow, int newColumn) {
+    row = newRow;
+    column = newColumn;
+  }
+
+  @override
+  String toString() {
+    return 'TileEntity{id: $id, value: ${pow(2, value)}, row: $row, column: $column}';
+  }
+}
